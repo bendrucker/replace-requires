309c59475c27a88a25b04e174c3e840410de4922
Ben Drucker <bvdrucker@gmail.com>
2017-04-24 11:05:34 -0700

    Update detective and allow minor versions
    Closes #5, Closes #4

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ test(function (t) {
   t.equal(replace('require("foo")', {foo: 'bar'}), 'bar')
   t.equal(replace('require("foo");require("bar")', {foo: 'bar', bar: 'baz'}), 'bar;baz')
   t.equal(replace('require("foo")', {}), 'require("foo")')
-
+  t.equal(replace('require("foo"); async function someFunction () {}', {foo: 'bar'}), 'bar; async function someFunction () {}')
   t.equal(replace('require("foo/bar")', {'foo/bar': 'baz'}), 'baz')
 
   t.end()
