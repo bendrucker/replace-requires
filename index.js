184db0bd1f4692884fea2aca16bb28b0ea9207bd
Ben Drucker <bvdrucker@gmail.com>
2022-04-21 11:01:05 -0700

    Travis -> GitHub Actions (#20)

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict'
 
-var detective = require('detective')
-var patch = require('patch-text')
-var hasRequire = require('has-require')
-var extend = require('xtend')
+const detective = require('detective')
+const patch = require('patch-text')
+const hasRequire = require('has-require')
+const extend = require('xtend')
 
 module.exports = function replaceRequires (code, replacements) {
-  var checker = new hasRequire.Checker(code)
-  var ids = Object.keys(replacements)
+  const checker = new hasRequire.Checker(code)
+  const ids = Object.keys(replacements)
   if (!ids.some(checker.has, checker)) return code
   return patch(code, detective
-    .find(code, {nodes: true})
+    .find(code, { nodes: true })
     .nodes
     .filter(requireLiteral)
     .map(function (node) {
-      return extend(node, {replacement: replacements[node.arguments[0].value]})
+      return extend(node, { replacement: replacements[node.arguments[0].value] })
     })
     .filter(function (node) {
       return node.replacement != null
     }))
-
 }
 
 function requireLiteral (node) {
-  var arg = node.arguments[0]
+  const arg = node.arguments[0]
   return arg && arg.type === 'Literal'
 }
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 'use strict'
 
-var test = require('tape')
-var replace = require('./')
+const test = require('tape')
+const replace = require('./')
 
 test(function (t) {
-  t.equal(replace('require("foo")', {foo: 'bar'}), 'bar')
-  t.equal(replace('require("foo");require("bar")', {foo: 'bar', bar: 'baz'}), 'bar;baz')
+  t.equal(replace('require("foo")', { foo: 'bar' }), 'bar')
+  t.equal(replace('require("foo");require("bar")', { foo: 'bar', bar: 'baz' }), 'bar;baz')
   t.equal(replace('require("foo")', {}), 'require("foo")')
   t.equal(replace('let n = require("n"); export { n }', {}), 'let n = require("n"); export { n }')
-  t.equal(replace('require("foo"); async function someFunction () {}', {foo: 'bar'}), 'bar; async function someFunction () {}')
-  t.equal(replace('require("foo/bar")', {'foo/bar': 'baz'}), 'baz')
+  t.equal(replace('require("foo"); async function someFunction () {}', { foo: 'bar' }), 'bar; async function someFunction () {}')
+  t.equal(replace('require("foo/bar")', { 'foo/bar': 'baz' }), 'baz')
 
   t.end()
 })
